c08aef885cbd7ca98e052c1ea959e96bd3d01e8f
anantstack <iamanantkumar007@gmail.com>
2026-04-16 02:09:48 +0530

    debug db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log("👉 DATABASE_URL:", process.env.DATABASE_URL);
 
 const db = mysql.createConnection(process.env.DATABASE_URL);
+
+
 
 db.connect((err) => {
   if (err) {
