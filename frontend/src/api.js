aecdb6583ab00ea47e7d1956774cd3af7617fde1
anantstack <iamanantkumar007@gmail.com>
2026-04-15 22:26:55 +0530

    frontend ready for deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://your-backend-url.onrender.com/api"
+ /* baseURL: "https://your-backend-url.onrender.com/api" */
+ baseURL: "https://volunteer-backend-yu6v.onrender.com/api" // ✅ LIVE BACKEND
 });
