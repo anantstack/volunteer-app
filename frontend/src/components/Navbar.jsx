4cb16fcfa6643ccc350fc24d39afa26cc47fea92
anantstack <iamanantkumar007@gmail.com>
2026-04-18 12:47:22 +0530

    fix API import

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { API } from "../api";
 
 export default function Navbar() {
   const nav = useNavigate();
