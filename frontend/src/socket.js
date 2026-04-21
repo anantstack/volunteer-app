69d83f5fff3513735755f67cb8624c17a6c12353
anantstack <iamanantkumar007@gmail.com>
2026-04-22 01:27:35 +0530

    fix socket redeclaration

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://volunteer-backend-yu6v.onrender.com", {
-  transports: ["polling", "websocket"], // 🔥 FIX
-  reconnection: true,
-  reconnectionAttempts: 5,
-  timeout: 10000
+  transports: ["polling", "websocket"]
 });
 
 export default socket;
